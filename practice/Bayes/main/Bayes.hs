4bf2e2733736699d823dcb1ddd73d2477f133bf9
Arthur Lui <luiarthur@gmail.com>
2017-01-03 21:29:34 -0800

    yes

--- practice/Bayes/main/Bayes.hs
+++ practice/Bayes/main/Bayes.hs
@@ -1,8 +1,25 @@
-import Text.Regex.Base
-import Text.Regex.TDFA -- cabal update; cabal install regex-tdfa
+--import Text.Regex.Base
+--import Text.Regex.TDFA -- cabal update; cabal install regex-tdfa
 
 module Bayes (
 ) where
 
 
-dummmy = 0
+-- dummmy = 0
+-- 
+-- let pattern = "(foo|bar)"
+-- let x = "foobarbaz,barbarbar" =~ pattern :: Int
+-- 
+-- let y = "foobarbaz,barbarbar" =~ pattern :: (MatchOffset, MatchLength)
+-- "foobarbaz,barbarbar" =~ pattern :: (MatchOffset, MatchLength)
+-- 
+-- "this is the pattern the I'm looking for" =~ "the" :: [(MatchOffset,MatchLength)]
+-- 
+-- "this is the pattern the I'm looking for" =~ "the" :: Int
+
+-- Another Version:
+
+import Text.Regex
+-- let x = subRegex (mkRegex "foo[0-9]+") "foo53187barfoofoo329713" "123"
+
+
