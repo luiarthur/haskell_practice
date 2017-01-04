643f9075025007e8adde126567f2653a6f0c464c
Arthur Lui <luiarthur@gmail.com>
2017-01-04 13:41:07 -0800

    yes

--- practice/Bayes/main/Bayes.hs
+++ practice/Bayes/main/Bayes.hs
@@ -1,25 +1,33 @@
 --import Text.Regex.Base
 --import Text.Regex.TDFA -- cabal update; cabal install regex-tdfa
+-- http://stackoverflow.com/questions/10243290/determining-matching-parenthesis-in-haskell
+-- http://www.serpentine.com/blog/2007/02/27/a-haskell-regular-expression-tutorial/
+-- https://hackage.haskell.org/package/regex-compat-0.95.1/docs/Text-Regex.html
+-- http://stackoverflow.com/questions/21594587/indices-of-all-matches-of-a-regex
 
 module Bayes (
 ) where
 
+braces :: String -> [(Int,Int)]
+braces = go 0 [] -- fn named go, which takes the starting idx and accumulator of paren idx []
+  where 
+    go _ []      []         = []
+    go _ (_:_)   []         = error "unbalanced braces!"
+    go j acc     ('{' : cs) =         go (j+1) (j:acc) cs
+    go j []      ('}' : cs) = error "unbalanced braces!"
+    go j (i:is)  ('}' : cs) = (i,j) : go (j+1) is      cs
+    go j acc     (c   : cs) =         go (j+1) acc     cs
 
--- dummmy = 0
--- 
--- let pattern = "(foo|bar)"
--- let x = "foobarbaz,barbarbar" =~ pattern :: Int
--- 
--- let y = "foobarbaz,barbarbar" =~ pattern :: (MatchOffset, MatchLength)
--- "foobarbaz,barbarbar" =~ pattern :: (MatchOffset, MatchLength)
--- 
--- "this is the pattern the I'm looking for" =~ "the" :: [(MatchOffset,MatchLength)]
--- 
--- "this is the pattern the I'm looking for" =~ "the" :: Int
+-- braces "abd{adsad}{def}"
 
--- Another Version:
+-- import Text.Regex
+-- import Text.Regex.Base
+-- import Data.Array ((!))
+-- let pat = mkRegex "[()]"
+-- splitRegex pat "abc(bla(beef))def" 
 
-import Text.Regex
--- let x = subRegex (mkRegex "foo[0-9]+") "foo53187barfoofoo329713" "123"
+-- let pat = makeRegex "[()]" :: Regex
+-- let m = matchAll pat "abc(bla(beef))def"
+-- let x = map (\(a,b) -> a :: Int) $ map(!0) m
 
 
--- practice/Bayes/test/test.hs
+++ practice/Bayes/test/test.hs
@@ -1,12 +1,18 @@
 import Test.HUnit
 import Bayes
+import System.IO
 
 test0 = TestCase ( assertBool "This should never show" True )
 test1 = TestCase ( assertEqual "This should never show test2" 1 (3-2) )
 
+-- test 2
+let path = "../resource/test.model"
+file <- readFile path
+
+braces file
+
 tests = TestList [ TestLabel "test1" test1,
                    TestLabel "test0" test0 ]
-
 main = do
   runTestTT tests
 
