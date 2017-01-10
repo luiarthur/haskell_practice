9eb841e6afc4ea0f95df475250446abac12cc314
Arthur Lui <luiarthur@gmail.com>
2017-01-10 11:22:42 -0800

    yes

--- practice/Bayes/main/Bayes.py
+++ practice/Bayes/main/Bayes.py
@@ -1,5 +1,16 @@
 import re
 
+rvSupport = {'Beta': 'unity', 
+             'Normal': 'real', 
+             'Gamma': 'positiveReal',
+             'InvGamma': 'positiveReal',
+             'DP': 'G0'}
+
+rvType = {'unity': 'double', 
+          'real': 'double',
+          'positiveReal': 'double',
+          'G0': 'G0'}
+
 
 def parseFile(path):
     f = open(path)
@@ -17,11 +28,40 @@ def getField(s, field):
     return re.search(field + "\{[^{]+\}", s).group()
 
 
+# match between ";"
+reElem = re.compile("(?<=;)[^;]+(?=;)")
+
+
+def getRcppFunArg(s):
+    data = getField(s, "Data")
+    data_elem = reElem.findall(data)
+    sep = map(lambda x: str.split(x,":"), data_elem)
+    param = [ e[0] for e in sep ]
+    paramType = [re.sub("vector","std::vector",e[1]) for e in sep]
+    args = map(lambda z: z[0] + " " + z[1], zip(param,paramType))
+    return str.join(", ", args)
+
+
+# match the distribution
+reDistr = re.compile("\w+(?=\()")
+
+# get G0 for DP
+reDPType = re.compile("(?<=G0=)\w+(?=\()")
+
+def getRcppReturnType(s):
+    prior = getField(s, "Prior")
+    prior_elem = reElem.findall(prior)
+    sep = map(lambda x: str.split(x,"~"), prior_elem)
+    distr = map(lambda x: reDistr.search(x[-1]).group() , sep)
+    # if any of them are DP, then redo those and get reDPType
+    # FIXME Stopped here.
+
+
+
 ## Tests:
 
 s = parseFile("../resource/test2.model")
+getRcppFunArg(s)
+getRcppReturnType(s)
 
 
-#def getParam(s):
-data = getField(s, "Data")
-datum = re.match(";(^;)+;", data) # FIXME
